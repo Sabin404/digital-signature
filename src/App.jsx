35fb783a2e43c236f9c68ada2a361ece0a00ccb0
Sabin <paudelsabin0@gmail.com>
2024-07-06 15:26:46 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState ,useRef} from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import SignatureCanvas from "react-signature-canvas";
 import Input from "./Components/Input";
@@ -8,7 +8,7 @@ function App() {
   const [bgcolor, setBgcolor] = useState('black');
   // console.log(bgcolor);
   const [pencolor, setPencolor] = useState('white')
-  const sigCanvas=useRef()
+  const sigCanvas = useRef()
 
   const backgroundcolor = (e) => {
     setBgcolor(e.target.value);
@@ -18,16 +18,32 @@ function App() {
     setPencolor(e.target.value)
   }
 
-  const clearDraw=()=>{
+  const clearDraw = () => {
     sigCanvas.current.clear();
+    const imageDiv = document.getElementsByClassName('image');
+    imageDiv[0].style.display = 'none';
+    // localStorage.clear()
+    console.log(sigCanvas);
   }
 
-  const downloadSignature=()=>{
-    const dataURL = sigCanvas.current.getTrimmedCanvas().toDataURL('image/png');
-    const link = document.createElement('a');
-    link.download = 'my-signature.png';
-    link.href = dataURL;
-    link.click();
+  const downloadSignature = () => {
+    if (sigCanvas.current.isEmpty()) {
+      alert('Please Signature :) ')
+    } else {
+      const dataURL = sigCanvas.current.getTrimmedCanvas().toDataURL('image/png');
+      const link = document.createElement('a');
+      link.download = 'my-signature.png';
+      link.href = dataURL;
+      link.click();
+      const image = document.getElementsByClassName('image');
+      const img = document.createElement('img')
+      img.src = dataURL;
+      img.classList.add("realimage");
+      image[0].appendChild(img);
+      image[0].style.display = 'block';
+      // localStorage.setItem('SignatureImage', dataURL)
+      // console.log(dataURL);
+    }
   }
   return (
     <>
@@ -44,7 +60,12 @@ function App() {
           canvasProps={{ width: 600, height: 300 }}
         />
       </div>
-      <Button Clear="Clear" Download="Download" clearDraw={clearDraw} download={downloadSignature}/>
+      <Button Clear="Clear" Download="Download" clearDraw={clearDraw} download={downloadSignature} />
+
+      <div className="image text-center bg-black  p-4 rounded-lg shadow-md">
+        <p className="text-lg font-semibold text-white mb-2">Your Signatures:</p>
+      </div>
+
     </>
   );
 }
